473fb1d53ce66ee37956177ca6b3b9130190d303
mstva <mustafaabdo468@gmail.com>
2020-07-28 15:30:29 +0200

    add functions to game class

---
+++ SnakeGame/Game.cpp
@@ -0,0 +1,30 @@
+#include "Game.h"
+
+Game::Game()
+{
+}
+
+bool Game::initGame()
+{
+    return false;
+}
+
+void Game::closeGame()
+{
+}
+
+void Game::runGame()
+{
+}
+
+void Game::processInput()
+{
+}
+
+void Game::updateGame()
+{
+}
+
+void Game::renderGame()
+{
+}
