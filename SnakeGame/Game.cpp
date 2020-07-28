9c018597c8e9d71ad47929fd5e90b8f3ec145bc8
mstva <mustafaabdo468@gmail.com>
2020-07-28 15:56:07 +0200

    create new window and new renderer

--- SnakeGame/Game.cpp
+++ SnakeGame/Game.cpp
@@ -9,6 +9,42 @@ Game::Game() :
 
 bool Game::initGame()
 {
+    // check SDL initialzation
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    // create the window
+    mWindow = SDL_CreateWindow(
+        "Snake Game",                      // title
+        SDL_WINDOWPOS_UNDEFINED,           // x position
+        SDL_WINDOWPOS_UNDEFINED,           // y position
+        WIDTH,                             // width
+        HIEGHT,                            // height
+        SDL_WINDOW_OPENGL                  // flag
+    );
+
+    // check window creating
+    if (mWindow == NULL)
+    {
+        SDL_Log("Unable to create SDL window: %s", SDL_GetError());
+        return false;
+    }
+
+    mRenderer = SDL_CreateRenderer(
+        mWindow,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    if (mRenderer == NULL)
+    {
+        SDL_Log("Unable to create SDL renderer: %s", SDL_GetError());
+        return false;
+    }
+
     return false;
 }
 
